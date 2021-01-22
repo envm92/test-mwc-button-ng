68e58017ff07101c988be88177bef41da461fe3a
Eri <envm92@gmail.com>
2021-01-22 10:51:18 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// Web components
+import '@material/mwc-button';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
